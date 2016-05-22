da749a2e26b8f70b60255ba1af696b847c1354a9
Tarik Bennett <tarik.bennett@gmail.com>
2016-05-21 22:20:12 -0700

    Add sub navigation

--- lessons/01-setting-up/index.js
+++ lessons/01-setting-up/index.js
@@ -3,15 +3,19 @@ import { render } from 'react-dom'
 import { Router, Route, hashHistory } from 'react-router'
 import About from './modules/About'
 import Repos from './modules/Repos'
+import Repo from './modules/Repo'
 import App from './modules/App'
 
 
 render(
   (
   <Router history={hashHistory}>
-    <Route path="/" component={App}/>
-    <Route path="about" component={About}/>
-    <Route path="repos" component={Repos}/>
+    <Route path="/" component={App}>
+      <Route path="/repos" component={Repos}>
+        <Route path="/repos/:userName/:repoName" component={Repo}/>
+      </Route>
+      <Route path="/about" component={About}/>
+    </Route>
   </Router>
   ), document.getElementById('app')
 )
--- lessons/01-setting-up/modules/Repos.js
+++ lessons/01-setting-up/modules/Repos.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router'
+import NavLink from './NavLink'
 
 export default React.createClass({
-
   render() {
-    return
+    return (
       <div>
-        Repos
+        <h2>Repos</h2>
+        <ul>
+          <li><NavLink to="/repos/reactjs/react-router">React Router</NavLink></li>
+          <li><NavLink to="/repos/facebook/react">React</NavLink></li>
+        </ul>
+        {this.props.children}
       </div>
+    )
   }
 })
